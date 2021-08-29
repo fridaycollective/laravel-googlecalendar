3184c961935f3184df5337680dd6b881336f1b6d
Dean Hopkins <deanhopkins@gmail.com>
2021-08-30 00:36:47 +0100

    Gcal services

---
+++ src/Services/Google/Calendar/GoogleCalendarService.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace FridayCollective\LaravelGoogleCalendar\Services\Google\Calendar;
+
+use App\Models\User;
+use Carbon\Carbon;
+use DateTime;
+use FridayCollective\LaravelGoogleCalendar\Models\UserCalendarIntegrationConfig;
+use Google\Service\Exception;
+use Google_Client;
+use Google_Service_Calendar;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Spatie\GoogleCalendar\GoogleCalendarFactory;
+
+class GoogleCalendarService
+{
+
+    protected $client;
+    protected $service;
+    protected $file;
+    protected $calendarConfig;
+
+    public function __construct(UserCalendarIntegrationConfig $calendarIntegrationConfig)
+    {
+        $this->calendarConfig = $calendarIntegrationConfig;
+        $allowJsonEncrypt = env('GOOGLE_ALLOW_JSON_ENCRYPT', false);
+        if ($allowJsonEncrypt) {
+            $accessToken = json_decode(decrypt($this->calendarConfig->config), true);
+        } else {
+            $accessToken = json_decode($this->calendarConfig->config, true);
+        }
+
+        $this->client = new Google_Client();
+        $accessToken['client_id'] = config('gmail.client_id');
+        $accessToken['client_secret'] = config('gmail.client_secret');
+        $email = $accessToken['email'] ?? null;
+        $this->client->setAuthConfig($accessToken);
+        $this->client->setAccessToken($accessToken);
+        if ($this->client->isAccessTokenExpired()) {
+            Log::debug("EXPIRED: Access token expired for calendar config " . $calendarIntegrationConfig->id);
+            $this->client->fetchAccessTokenWithRefreshToken($accessToken['refresh_token']);
+            $accessToken = $this->client->getAccessToken();
+            $accessToken['email'] = $email;
+            $this->saveAccessToken($this->calendarConfig, $accessToken);
+        } else {
+            Log::debug("FOUND: Access token for calendar config " . $calendarIntegrationConfig->id);
+        }
+        $accessToken['client_id'] = config('gmail.client_id');
+        $accessToken['client_secret'] = config('gmail.client_secret');
+        $this->client->setAuthConfig($accessToken);
+        $this->client->setAccessToken($accessToken);
+        $this->service = new Google_Service_Calendar($this->client);
+    }
+
+
+    private function saveAccessToken(CalendarIntegrationConfig $dbCalendarConfig, array $config)
+    {
+        $allowJsonEncrypt = env('GOOGLE_ALLOW_JSON_ENCRYPT', false);
+        if ($allowJsonEncrypt) {
+            $accessToken = json_encode(encrypt($config), true);
+        } else {
+            $accessToken = json_encode($config, true);
+        }
+
+        $dbCalendarConfig->config = $accessToken;
+        $dbCalendarConfig->save();
+    }
+
+
+    public function subscribeToCalendarNotifications($calendarId) {
+        /* @TODO THIS MUST BE PER CALENDAR AND LOGGED PER CALENDAR IN MODEL */
+        $channelConfig = new \Google_Service_Calendar_Channel();
+        $channelConfig->setId('CRANK_CRM_CAL_CONFIG_ID_' . $this->calendarConfig->id);
+        $channelConfig->setType('web_hook');
+        $channelConfig->setAddress(env('APP_URL') . '/api/webhooks/google-calendar');
+
+        $response = $this->service->events->watch($calendarId, $channelConfig);
+
+        $this->calendarConfig->google_notification_channel_id = $response->getId();
+        $this->calendarConfig->google_notification_resource_id = $response->getResourceId();
+        $this->calendarConfig->google_notification_channel_expiration = $response->getExpiration();
+        $this->calendarConfig->save();
+    }
+
+    public function unsubscribeFromCalendarNotifications() {
+        /* @TODO THIS MUST BE PER CALENDAR AND LOGGED PER CALENDAR IN MODEL */
+        $channelConfig = new \Google_Service_Calendar_Channel();
+        $channelConfig->setId($this->calendarConfig->google_notification_channel_id);
+        $channelConfig->setResourceId($this->calendarConfig->google_notification_resource_id);
+
+        $this->service->channels->stop($channelConfig);
+
+        $this->calendarConfig->google_notification_channel_id = null;
+        $this->calendarConfig->google_notification_resource_id = null;
+        $this->calendarConfig->google_notification_channel_expiration = null;
+        $this->calendarConfig->save();
+    }
+
+
+
+    public function getCalendarList()
+    {
+        $calendarList = $this->service->calendarList->listCalendarList();
+
+        return $calendarList;
+    }
+
+
+    /*
+     * @link https://developers.google.com/google-apps/calendar/v3/reference/events/list
+     */
+    public function listEvents(User $user, Carbon $startDateTime = null, Carbon $endDateTime = null, array $queryParameters = [])
+    {
+        $calendarConfig = $user->calendarIntegrationConfig;
+        $syncToken = $calendarConfig->sync_token;
+
+        $retCollection = new Collection();
+        $pageToken = null;
+        $continue = true;
+
+        while ($continue) {
+            if ($calendarConfig->status === 'active') {
+                try {
+                    $defaultParams = ['singleEvents' => true,];
+
+                    //if we have a sync token do an incremental sync otherwise do a full sync
+                    if ($syncToken) {
+                        $defaultParams['syncToken'] = $syncToken;
+                    } else {
+                        $startDateTime = $startDateTime ?? Carbon::now()->startOfDay();
+                        $defaultParams['timeMin'] = $startDateTime->format(DateTime::RFC3339);
+                        $endDateTime = $endDateTime ?? Carbon::now()->addYear()->endOfDay();
+                        $defaultParams['timeMax'] = $endDateTime->format(DateTime::RFC3339);
+                    }
+
+                    $optParams = array_merge($defaultParams, $queryParameters);
+
+                    if ($pageToken) {
+                        $optParams['pageToken'] = $pageToken;
+                    }
+                    $eventResponse = $this->service->events->listEvents($this->calendarId, $optParams);
+                    if (!$eventResponse) {
+                        return null;
+                    }
+
+
+                    foreach ($eventResponse->getItems() as $event) {
+                        $retCollection->push($event);
+                    }
+
+                    $pageToken = $eventResponse->nextPageToken;
+                    $syncToken = $eventResponse->nextSyncToken;
+                    $calendarConfig->sync_token = $syncToken;
+                    $calendarConfig->save();
+
+                    if (!$pageToken) {
+                        $continue = false;
+                    }
+                } catch (Exception $e) {
+                    if ($e->getCode() === 410) {
+                        if (sizeof(array_filter($e->getErrors(), function ($error) {
+                                return $error['reason'] === "fullSyncRequired";
+                            })) > 0) {
+                            Log::info("Refreshing sync token...");
+                            $calendarConfig->sync_token = null;
+                            $calendarConfig->save();
+                            //try again
+                            $syncToken = $calendarConfig->sync_token;
+                            $pageToken = null;
+                            $retCollection = new Collection();
+                        } else {
+                            //Log::error($e);
+                        }
+                    } else if ($e->getCode() === 404) {
+                        if (sizeof(array_filter($e->getErrors(), function ($error) {
+                                return $error['reason'] === "notFound";
+                            })) > 0) {
+                            Log::error("GOOGLE CALENDAR 404 NOT FOUND FOR USER " . $user->id);
+                        } else {
+                            //Log::error($e->getMessage());
+                        }
+                    } else if (strpos($e->getMessage(), 'Token has been expired or revoked') !== false) {
+                        $calendarConfig->status = 'revoked_expired';
+                        $calendarConfig->save();
+
+                        Log::debug("Expired calendar integration for user: " . $user->id);
+                    } else {
+                        //Log::error($e);
+                    }
+                }
+            }
+        }
+
+
+        return $retCollection;
+    }
+
+    public function insertEvent($event, $calendarId = 'primary')
+    {
+        return $this->service->events->insert($calendarId, $event);
+    }
+
+
+    public function deleteEvent($eventId, $calendarId = 'primary')
+    {
+        return $this->service->events->delete($calendarId, $eventId);
+    }
+
+    public function getEvent($eventId, $calendarId = 'primary')
+    {
+        try {
+            return $this->service->events->get($calendarId, $eventId);
+        } catch (Exception $e) {
+            if ($e->getCode() === 404) {
+                if (sizeof(array_filter($e->getErrors(), function ($error) {
+                        return $error['reason'] === "notFound";
+                    })) > 0) {
+                    Log::info("Google calendar event not found");
+                } else {
+                    Log::error($e);
+                }
+            }
+        }
+    }
+
+    public function updateEvent($event, $calendarId = 'primary')
+    {
+        return $this->service->events->update('primary', $event->getId(), $event);
+    }
+}
---
+++ src/Services/Google/Calendar/GoogleEvent.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace FridayCollective\LaravelGoogleCalendar\Services\Google\Calendar;
+
+use App\Models\User;
+use DateTime;
+use Carbon\Carbon;
+use Google_Service_Calendar_Event;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Google_Service_Calendar_EventDateTime;
+use Illuminate\Support\Str;
+
+class GoogleEvent
+{
+    /** @var \Google_Service_Calendar_Event */
+    public $googleEvent;
+
+    /** @var string */
+    protected $calendarId;
+
+    /** @var array */
+    protected $attendees;
+
+    protected $user;
+
+    protected $googleService;
+
+    public function __construct($user_id)
+    {
+        $this->user = User::find($user_id);
+        $this->attendees = [];
+        $this->googleService = new GoogleCalendarService($this->user->id);
+        $this->googleEvent = new Google_Service_Calendar_Event;
+    }
+
+    public function createFromGoogleCalendarEvent(Google_Service_Calendar_Event $googleEvent, $calendarId)
+    {
+        $event = new GoogleEvent($this->user->id);
+
+        $event->googleEvent = $googleEvent;
+        $event->calendarId = $calendarId;
+
+        return $event;
+    }
+
+    public function get(Carbon $startDateTime = null, Carbon $endDateTime = null, array $queryParameters = [], string $calendarId = null) : Collection
+    {
+
+        $googleEvents = $this->googleService->listEvents($this->user, $startDateTime, $endDateTime, $queryParameters);
+        if(!$googleEvents) {
+            return null;
+        }
+        return $googleEvents->map(function (Google_Service_Calendar_Event $event) use ($calendarId) {
+                return $this->createFromGoogleCalendarEvent($event, $calendarId);
+            })
+            ->values();
+    }
+
+    public function __get($name)
+    {
+        $name = $this->getFieldName($name);
+
+        if ($name === 'sortDate') {
+            return $this->getSortDate();
+        }
+
+        $value = Arr::get($this->googleEvent, $name);
+
+        if (in_array($name, ['start.date', 'end.date']) && $value) {
+            $value = Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
+        }
+
+        if (in_array($name, ['start.dateTime', 'end.dateTime']) && $value) {
+            $value = Carbon::createFromFormat(DateTime::RFC3339, $value);
+        }
+
+        return $value;
+    }
+
+    public function __set($name, $value)
+    {
+        $name = $this->getFieldName($name);
+
+        if (in_array($name, ['start.date', 'end.date', 'start.dateTime', 'end.dateTime'])) {
+            $this->setDateProperty($name, $value);
+
+            return;
+        }
+        Arr::set($this->googleEvent, $name, $value);
+    }
+
+    public function exists(): bool
+    {
+        return $this->id != '';
+    }
+
+    public function isAllDayEvent(): bool
+    {
+        return is_null($this->googleEvent['start']['dateTime']);
+    }
+
+    public function save()
+    {
+        $method = $method ?? ($this->exists() ? 'updateEvent' : 'insertEvent');
+        $this->googleEvent->setAttendees($this->attendees);
+        return $this->googleService->$method($this->googleEvent);
+    }
+
+    public function update(array $attributes, $optParams = []): self
+    {
+        foreach ($attributes as $name => $value) {
+            $this->$name = $value;
+        }
+
+        return $this->save('updateEvent', $optParams);
+    }
+
+    public function delete(string $eventId = null)
+    {
+        $this->googleService->deleteEvent($eventId);
+    }
+
+    public function getEvent($eventId = null)
+    {
+        $event = $this->googleService->getEvent($eventId);
+        if ($event) {
+            return $this->createFromGoogleCalendarEvent($event, null);
+        }
+    }
+
+    public function addAttendee(array $attendees)
+    {
+        $this->attendees[] = $attendees;
+    }
+
+    public function getSortDate(): string
+    {
+        if ($this->startDate) {
+            return $this->startDate;
+        }
+
+        if ($this->startDateTime) {
+            return $this->startDateTime;
+        }
+
+        return '';
+    }
+
+    protected function setDateProperty(string $name, Carbon $date)
+    {
+        $eventDateTime = new Google_Service_Calendar_EventDateTime;
+
+        if (in_array($name, ['start.date', 'end.date'])) {
+            $eventDateTime->setDate($date->format('Y-m-d'));
+            $eventDateTime->setTimezone($date->getTimezone());
+        }
+
+        if (in_array($name, ['start.dateTime', 'end.dateTime'])) {
+            $eventDateTime->setDateTime($date->format(DateTime::RFC3339));
+            $eventDateTime->setTimezone($date->getTimezone());
+        }
+
+        if (Str::startsWith($name, 'start')) {
+            $this->googleEvent->setStart($eventDateTime);
+        }
+
+        if (Str::startsWith($name, 'end')) {
+            $this->googleEvent->setEnd($eventDateTime);
+        }
+    }
+
+    protected function getFieldName(string $name): string
+    {
+        return [
+                   'name' => 'summary',
+                   'description' => 'description',
+                   'startDate' => 'start.date',
+                   'endDate' => 'end.date',
+                   'startDateTime' => 'start.dateTime',
+                   'endDateTime' => 'end.dateTime',
+               ][$name] ?? $name;
+    }
+}
