64d0a8b8189d65d23e9208a4624785c501b7f0b5
Dean Hopkins <deanhopkins@gmail.com>
2021-10-06 01:03:33 +0100

    Fix

--- src/LaravelGoogleCalendarServiceProvider.php
+++ src/LaravelGoogleCalendarServiceProvider.php
@@ -38,7 +38,6 @@ class LaravelGoogleCalendarServiceProvider extends ServiceProvider
                 break;
         }
 
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 }
